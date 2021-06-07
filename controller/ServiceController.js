a491aab35b7d91daa9d558b720aa26a29d2f753a
mkhorin <maksimovichu@gmail.com>
2021-06-07 17:49:52 +1000

    Add navigation search

--- controller/ServiceController.js
+++ controller/ServiceController.js
@@ -16,6 +16,21 @@ module.exports = class ServiceController extends Base {
         const menu = this.spawn(SideMenu, {view});
         this.send(await menu.renderItems(node.children, node.section));
     }
+
+    async actionNavSearch () {
+        const section = this.navMeta.getSection('main');
+        const value = this.getPostParam('search');
+        if (typeof value !== 'string' || value.length < 2 || value.length > 16) {
+            throw new BadRequest('Invalid search value');
+        }
+        const nodes = section.search(value);
+        if (!nodes.length) {
+            return this.send();
+        }
+        const view = this.createView();
+        const menu = this.spawn(SideMenu, {view});
+        this.send(await menu.renderItems(nodes, section));
+    }
 };
 module.exports.init(module);
 
